d5f9f2fdbe02fcffdba86943b7c69463ce3707e0
Isao Yoshikoshi <latica.jp@gmail.com>
2019-09-05 07:51:27 +0900

    chore: inline-source-map: 通常は devServer なので source-map になっているが今回は dev ビルドしたものをアプリに添付したかったので

--- config/webpack.config.goqoo.js
+++ config/webpack.config.goqoo.js
@@ -1,20 +1,3 @@
+const merge = require('webpack-merge')
 const config = require('../.goqoo/webpack.config.base')
-
-// Edit the "config" object as you like.
-// Do not edit ".goqoo/webpack.config.base"
-
-// ex. Add Loaders
-// config.module.rules.push({
-//   test: /\.ext$/,
-//   loader: ['foo-loader', 'bar-loader'],
-// })
-
-// ex. Add Plugins
-// config.plugins.push(new foobarPlugin())
-
-// ex. Change webpack-dev-server Settings
-// config.devServer.hot = true
-
-// And more!
-
-module.exports = config
+module.exports = merge(config, { devtool: 'inline-source-map' })
